99977cb2d5231a0974ef941417bc0b5170165ca2
KeitaIHARA <keita18060808@gmail.com>
2025-12-09 21:38:46 +0900

    resultの解析

---
+++ linear_interpolation.py
@@ -0,0 +1,29 @@
+import numpy as np
+from scipy.interpolate import interp1d
+import math
+
+def linear_interpolation(text_name: str):
+
+    # --- テキスト読み込み ---
+    data = np.loadtxt(text_name, delimiter=",")  # x,y が2列の場合
+    x = data[:,0] 
+    y = data[:,1]
+
+    # --- 線形補間関数を作成 ---
+    f = interp1d(x, y, kind='linear')  # 線形補間
+
+    # --- 補間するx座標を生成、xの最小値,最大値を小数第4位で切り上げ、切り下げした範囲で0.001mごと ---
+    min_x_round = math.ceil(min(x) * (10**3)) / (10**3)
+    max_x_round = math.floor(max(x) * (10**3)) / (10**3)
+    x_new = np.linspace(min_x_round, max_x_round, round((max_x_round - min_x_round) / 0.001 + 1))
+
+    # --- 補間実行 ---
+    y_new = f(x_new)
+
+    # --- 最初の地点が（0，0）になるように平行移動 ---
+    x_new_offset = x_new - np.full(round((max_x_round - min_x_round) / 0.001 + 1), min_x_round)
+    y_new_offset = y_new - np.full(round((max_x_round - min_x_round) / 0.001 + 1), y_new[0])
+
+    return y_new_offset
+
+linear_interpolation("./middle_airplane_trajectories/middle_airplane_trajectory_no1.txt")
